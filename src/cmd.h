03692cc2c09587babe1d099c04ce0d21b5d52ebb
PoCTo <reinkarn@gmail.com>
2010-11-12 00:11:43 +0300

    Cmd and List structures

---
+++ src/cmd.h
@@ -0,0 +1,24 @@
+#define LINK_SEMICOLON   0
+#define LINK_BACKGROUND  10
+#define LINK_AND         20
+#define LINK_OR          21
+#define LINK_PIPE        31
+
+typedef enum {
+	FALSE, 
+	TRUE
+} bool;
+
+typedef struct List{
+	char* data;
+	struct List* next;
+} List;
+
+typedef struct Cmd {
+	char* exec; 
+	struct List* params;
+	struct List* in;
+	struct List* out;
+  struct Cmd* next;
+  int linkType;
+} Cmd;
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "cmd.h"
+
+
+int main(int argc,char** argv){
+	
+	return 0;
+}
