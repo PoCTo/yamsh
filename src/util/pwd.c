4ecf3260cf8bdd557553ce4e4dca1d969ea16cbd
PoCTo <reinkarn@gmail.com>
2010-11-22 01:54:42 +0300

    Added error handling to pwd

--- src/util/pwd.c
+++ src/util/pwd.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(){
     char* buf=NULL; int size=8;
@@ -8,6 +9,10 @@ int main(){
     while ((cwd=getcwd(buf,size))==NULL){
         free(buf); buf=NULL;
         size*=2;
+        if (errno!=ERANGE){
+            perror("pwd");
+            return 1;
+        }
     }
     printf("%s\n",cwd);
     free(cwd);
